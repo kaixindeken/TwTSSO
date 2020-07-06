87c81a31b5b0154c28a5e05b22b01a814c74adb0
zhangruian <zra981217@163.com>
2020-07-06 22:05:13 +0800

    change auth method

--- src/main/java/com/github/kaixindeken/TwTSSO/curl/CurlOptionType.java
+++
@@ -1,8 +0,0 @@
-package com.github.kaixindeken.TwTSSO.curl;
-
-public interface CurlOptionType {
-	int CURLOPTTYPE_LONG = 0;
-	int CURLOPTTYPE_OBJECTPOINT = 10000;
-	int CURLOPTTYPE_FUNCTIONPOINT = 20000;
-	int CURLOPTTYPE_OFF_T = 30000;
-}
--- target/apidocs/package-search-index.js
+++ target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"com.github.kaixindeken.TwTSSO","h":"","u":""},{"l":"com.github.kaixindeken.TwTSSO.curl","h":"","u":""},{"l":"com.github.kaixindeken.TwTSSO.curl.impl","h":"","u":""},{"l":"com.github.kaixindeken.TwTSSO.standard","h":"","u":""},{"l":"所有程序包","h":"","u":"allpackages-index.html"}]
+packageSearchIndex = [{"l":"cn.edu.twt.TwTSSO","h":"","u":""},{"l":"cn.edu.twt.TwTSSO.https","h":"","u":""},{"l":"cn.edu.twt.TwTSSO.standard","h":"","u":""},{"l":"所有程序包","h":"","u":"allpackages-index.html"}]
